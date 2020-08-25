9ba520c3ce97d1aebb841d559b29eab8eaef7b6c
Nazip <nazipm@mail.ru>
2020-08-25 21:54:17 +0300

    delete model

--- lib/string_util.dart
+++
@@ -1,7 +0,0 @@
-mixin UserUtils {
-  capitalize(String str) {
-    if (str == null || str.length == 0) return str;
-    return str[0].toUpperCase() +
-        (str.length > 1 ? str.substring(1).toLowerCase() : '');
-  }
-}
